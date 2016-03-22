98a2ff01e6e3ae6b3c7e14caa48b4832eca7be9d
mdlawrenceusa <mdlawrence@me.com>
2016-03-22 13:58:50 -0400

    Up to 5.11

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,14 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def show
@@ -7,10 +16,24 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
+      @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
+  end
+
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   private
